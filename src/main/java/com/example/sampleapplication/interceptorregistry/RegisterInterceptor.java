f56a30acf34354c447e334890f0ee6bd7c5d3b3c
Utkarsh Verma <utkarsh.verma@sixt.com>
2022-03-19 19:21:11 +0530

    final commit

--- src/main/java/com/example/sampleapplication/interceptorregistry/RegisterInterceptor.java
+++ src/main/java/com/example/sampleapplication/interceptorregistry/RegisterInterceptor.java
@@ -3,6 +3,8 @@ package com.example.sampleapplication.interceptorregistry;
 
 import com.example.sampleapplication.communicationinterceptor.CommunicationInterceptor;
 import com.example.sampleapplication.ratelimiter.interceptor.RateLimiterInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -11,15 +13,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class RegisterInterceptor implements WebMvcConfigurer {
 
+    @Autowired
+    ApplicationContext ctx;
 
-    @Bean
-    RateLimiterInterceptor getRateLimitInterceptor(){
-        return new RateLimiterInterceptor();
-    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry){
         registry.addInterceptor(new CommunicationInterceptor());
-        registry.addInterceptor(getRateLimitInterceptor());
+        registry.addInterceptor(new RateLimiterInterceptor(ctx));
     }
 }
